35e36ad1682442db97d7f43679c69022a99973c1
Enoch Alvanez <142016994+Maggie175@users.noreply.github.com>
2023-08-15 10:04:46 +0800

    Update Multiply.py

--- Multiply.py
+++ Multiply.py
@@ -8,4 +8,17 @@ def product(a,b):
           
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
-print("Product is: ",product(a,b))
+print("Product is: ",product(a,b)) 
+
+def product(a,b):
+     if(a<b):
+         return product(b,a)
+     elif(b!=0):
+         return(a+product(a,b-1))
+     else:
+          return 0
+          
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Product is: ",product(a,b)) 
+
